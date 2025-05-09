72c9ed232d4482cabedb8cf701d3f65576472cb7
KimDongKyu <kowbiopsdrh@gmail.com>
2025-05-09 15:43:40 +0900

    업로드

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,43 +1,43 @@
-let currentImageIndex = 0;
-const images = [
+let currentShopnovaIndex = 0;
+const shopnovaImages = [
   "img/shopnova.jpg",
   "img/shopnova1.jpg",
   "img/shopnova2.jpg",
   "img/shopnova3.jpg"
 ];
 
-function openModal(projectName) {
-  document.getElementById('modal-' + projectName).style.display = 'block';
-  showImage(currentImageIndex);
-}
-
 function closeModal(projectName) {
   document.getElementById('modal-' + projectName).style.display = 'none';
 }
 
-function showImage(index) {
-  const sliderImage = document.getElementById('slider-image');
-  sliderImage.src = images[index];
+function openModal(projectName) {
+  document.getElementById('modal-' + projectName).style.display = 'block';
+  showImageShopnova(currentShopnovaIndex);
 }
 
-function prevImage() {
-  currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
-  showImage(currentImageIndex);
+function showImageShopnova(index) {
+  const sliderImage = document.getElementById('slider-image-shopnova');
+  sliderImage.src = shopnovaImages[index];
 }
 
-function nextImage() {
-  currentImageIndex = (currentImageIndex + 1) % images.length;
-  showImage(currentImageIndex);
+function prevImageShopnova() {
+  currentShopnovaIndex = (currentShopnovaIndex - 1 + shopnovaImages.length) % shopnovaImages.length;
+  showImageShopnova(currentShopnovaIndex);
 }
 
+function nextImageShopnova() {
+  currentShopnovaIndex = (currentShopnovaIndex + 1) % shopnovaImages.length;
+  showImageShopnova(currentShopnovaIndex);
+}
+
+
+let currentCampingIndex = 0;
 const campingImages = [
   'img/CampingReservation.jpg',
   'img/CampingReservation1.jpg',
 ];
 
-let currentCampingIndex = 0;
-
-function prevImageCamping() {
+/* function prevImageCamping() {
   currentCampingIndex = (currentCampingIndex - 1 + campingImages.length) % campingImages.length;
   document.getElementById('slider-image-camping').src = campingImages[currentCampingIndex];
 }
@@ -45,6 +45,23 @@ function prevImageCamping() {
 function nextImageCamping() {
   currentCampingIndex = (currentCampingIndex + 1) % campingImages.length;
   document.getElementById('slider-image-camping').src = campingImages[currentCampingIndex];
+}
+*/
+
+function showImageCamping(index) {
+  const sliderImage = document.getElementById('slider-image-camping');
+  sliderImage.src = campingImages[index];
+}
+
+function prevImageCamping() {
+  currentShopnovaIndex = (currentCampingIndex - 1 + campingImages.length) % campingImages.length;
+  showImageCamping(currentCampingIndex);
+}
+
+
+function nextImageCamping() {
+  currentCampingIndex = (currentCampingIndex + 1) % campingImages.length;
+  showImageCamping(currentCampingIndex);
 }
 
 
