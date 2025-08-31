84630448994d8b1bf42d5617ee91835b4a7333ec
KimDongKyu <kowbiopsdrh@gmail.com>
2025-08-31 16:31:35 +0900

    일부 수정

--- js/portfolio.js
+++ js/portfolio.js
@@ -5,3 +5,18 @@ function openModal(id) {
   function closeModal(id) {
     document.getElementById('modal-' + id).classList.add('hidden');
   }
+
+const toggleBtn = document.getElementById("toggleBtn");
+  const intro = document.getElementById("intro");
+  let isOpen = false;
+
+  toggleBtn.addEventListener("click", () => {
+    if (!isOpen) {
+      intro.style.maxHeight = intro.scrollHeight + "px"; // 펼치기
+      toggleBtn.textContent = "▲";
+    } else {
+      intro.style.maxHeight = "0px"; // 닫기
+      toggleBtn.textContent = "▼";
+    }
+    isOpen = !isOpen;
+  });
